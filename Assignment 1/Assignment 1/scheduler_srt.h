af50991c2f06b5e94eeb75313a3c7283b6eeaeaf
steviewanger <steviewanger@gmail.com>
2015-02-13 16:36:07 -0800

    Added scheduler_srt.c and .h

---
+++ Assignment 1/Assignment 1/scheduler_srt.h
@@ -0,0 +1,11 @@
+#ifndef SHORTEST_REMAINING_TIME_H
+#define SHORTEST_REMAINING_TIME_H
+
+#include "system_headers.h"
+#include "helper_functions.h"
+#include "simulated_process.h"
+#include "ready_queue.h"
+
+void schedule_SRT(struct ready_queue readyQueue);
+
+#endif
