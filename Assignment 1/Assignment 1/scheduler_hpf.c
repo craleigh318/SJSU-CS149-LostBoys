6bc4ba3286dc11aa9622502d0c1e22601241e448
Will2015 <Will2015@WilliamsLaptop>
2015-02-14 22:10:44 -0800

    adding 2 method declaration to scheduler_hpf.h

--- Assignment 1/Assignment 1/scheduler_hpf.c
+++ Assignment 1/Assignment 1/scheduler_hpf.c
@@ -1,51 +1,59 @@
 #include "simulated_process.h"
 #include "scheduler_hpf.h"
+#include "ready_queue.h"
 
 void schedule_hpf(struct ready_queue readyQueue) {
-//    // 1st sort simulated processes in arrival time order - using Bubblesort
-//    int out, in;
-//    int queueSize = 26;
-//    struct ready_queue sorted = readyQueue; // Store runningQueue parameter into the sorted variable
-//    for (out = 0; out < sorted.length; out++) {
-//        for (in = 0; in < sorted.length - 1; in++) {
-//            if (sorted.processes[in].arrivalTime
-//                > sorted.processes[out + 1].arrivalTime) {
-//                struct simulated_process arrival = sorted.processes[in + 1];
-//                sorted.processes[in + 1] = sorted.processes[out];
-//                sorted.processes[out] = arrival;
-//            }
-//        }
-//    }
     
-    // 2nd create 4 priority queues label 1-4 priority && empty everything in it with 1 - highest
-    struct ready_queue priority1 = new_ready_queue(queueSize);
-    struct ready_queue priority2 = new_ready_queue(queueSize);
-    struct ready_queue priority3 = new_ready_queue(queueSize);
-    struct ready_queue priority4 = new_ready_queue(queueSize);
-    
-    // 3rd With 'sort' completed, take the 1st element/process from sort, determine its priority and place it into the matching priority queue.
-    
-    for (int i = 0; i <= queueSize; i++) // Go through entire sort
+    // 1st create 4 priority queues label 1-4 priority && empty everything in it. 1 = Highest
+    struct ready_queue priority1 = new_ready_queue(readyQueue.length);
+    // Initialize priority1 with some generated simulated process
+    // We have to get rid of the randomly generated process to nothing
+    for(int i = 0; i < readyQueue.length; i++)
     {
-        if (sorted.processes[i].priority == 1) // Get the process within the sort and see if the process's priority matches the priority's queue number
+    	priority1[i] = 0;
+    }
+
+    struct ready_queue priority2 = new_ready_queue(readyQueue.queueSize);
+    for(int j = 0; j < readyQueue.length; j++)
+    {
+        priority2[j] = 0;
+    }
+
+    struct ready_queue priority3 = new_ready_queue(readyQueue.queueSize);
+    for(int k = 0; k < readyQueue.length; k++)
+    {
+        priority3[k] = 0;
+    }
+
+    struct ready_queue priority4 = new_ready_queue(readyQueue.queueSize);
+    for(int k = 0; k < readyQueue.length; k++)
+    {
+        priority4[k] = 0;
+    }
+    
+    // 2nd with the place
+    
+    for (int i = 0; i <= readyQueue.length; i++) // Go through entire sort
+    {
+        if (readyQueue.processes[i].priority == 1) // Get the process within the sort and see if the process's priority matches the priority's queue number
         {
             
-            add_ready_queue_process(&priority1, sorted.processes[i]); // if their process's priority number is equal to 1, then add that process into the priority1 queue
-            remove_ready_queue_process(&sorted, i); // Since that priority is placed into the priority1 queue , remove it from the sort queue
-        } else if (sorted.processes[i].priority == 2) {
-            add_ready_queue_process(&priority2, sorted.processes[i]);
-            remove_ready_queue_process(&sorted, i);
-        } else if (sorted.processes[i].priority == 3) {
-            add_ready_queue_process(&priority3, sorted.processes[i]);
-            remove_ready_queue_process(&sorted, i);
+            add_ready_queue_process(&priority1, readyQueue.processes[i]); // if their process's priority number is equal to 1, then add that process into the priority1 queue
+            remove_ready_queue_process(&readyQueue, i); // Since that priority is placed into the priority1 queue , remove it from the sort queue
+        } else if (readyQueue.processes[i].priority == 2) {
+            add_ready_queue_process(&priority2, readyQueue.processes[i]);
+            remove_ready_queue_process(&readyQueue, i);
+        } else if (readyQueue.processes[i].priority == 3) {
+            add_ready_queue_process(&priority3, readyQueue.processes[i]);
+            remove_ready_queue_process(&readyQueue, i);
         } else // else the process's priority is 4
         {
-            add_ready_queue_process(&priority4, sorted.processes[i]);
-            remove_ready_queue_process(&sorted, i);
+            add_ready_queue_process(&priority4, readyQueue.processes[i]);
+            remove_ready_queue_process(&readyQueue, i);
         }
     }
     
-    // 4th after processes are sorted into their priority queue based on their priority number
+    // 3rd after processes are sorted into their priority queue based on their priority number
     // Send the new priority queues to the preemptive (Round robin) & nonpreemptive (FCFS) methods.
     preemptive(priority1, priority2, priority3, priority4);
     nonpreemptive(priority1, priority2, priority3, priority4);
--- Assignment 1/Assignment 1/scheduler_hpf.h
+++ Assignment 1/Assignment 1/scheduler_hpf.h
@@ -8,4 +8,15 @@
  */
 void schedule_hpf(struct ready_queue readyQueue);
 
+/*
+ Sending priority queues (all 4) to FCFS
+ */
+void nonpreemptive(struct ready_queue readyQueue1,struct ready_queue readyQueue2,struct ready_queue readyQueue3,struct ready_queue readyQueue4);
+
+/*
+ Sending priority queues (all 4) to Round Robin for 1 quantum
+ */
+
+void preemptive(struct ready_queue readyQueue1,struct ready_queue readyQueue2,struct ready_queue readyQueue3,struct ready_queue readyQueue4);
+
 #endif /* defined(__Assignment_1__scheduler_hpf__) */
