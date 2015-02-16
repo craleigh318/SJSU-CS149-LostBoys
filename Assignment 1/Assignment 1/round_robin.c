e621f6f1442a65c190d20b785d59a16d3c932725
Christopher Raleigh <craleigh318@comcast.net>
2015-02-16 12:00:46 -0800

    Changed RR for global statistics.

--- Assignment 1/Assignment 1/round_robin.c
+++ Assignment 1/Assignment 1/round_robin.c
@@ -8,60 +8,61 @@
 #include "round_robin.h"
 #include "helper_functions.h"
 #include "scheduler_statistics.h"
+#include "global_statistics.h"
 
 
 int next_process(struct ready_queue readyQueue, float timeElapsed, int index) {
-	int i = index+1;
-	while(i != index) {
-		if(i == readyQueue.length)
-		   i = 0;
-		else {
-			if(readyQueue.processes[i].expectedRunTime > 0.0 && readyQueue.processes[i].arrivalTime <= timeElapsed)
-						return i;
-			i++;
-		}
-	}
-	return i;
+    int i = index+1;
+    while(i != index) {
+        if(i == readyQueue.length)
+            i = 0;
+        else {
+            if(readyQueue.processes[i].expectedRunTime > 0.0 && readyQueue.processes[i].arrivalTime <= timeElapsed)
+                return i;
+            i++;
+        }
+    }
+    return i;
 }
 
 int is_complete(struct ready_queue readyQueue, int index) {
-	if (readyQueue.processes[index].expectedRunTime <= 0.0)
-		return 1;
-	else
-		return 0;
+    if (readyQueue.processes[index].expectedRunTime <= 0.0)
+        return 1;
+    else
+        return 0;
 }
 
 int is_ready(struct ready_queue readyQueue, int index, float timeElapsed) {
-	if(readyQueue.processes[index].expectedRunTime > 0.0 && readyQueue.processes[index].arrivalTime <= timeElapsed)
-		return 1;
-	else
-		return 0;
+    if(readyQueue.processes[index].expectedRunTime > 0.0 && readyQueue.processes[index].arrivalTime <= timeElapsed)
+        return 1;
+    else
+        return 0;
 }
 
 int ready_processes(struct ready_queue readyQueue, float timeElapsed) {
-	int i;
-	int readyProcesses = 0;
-	for(i = 0; i < readyQueue.length; i++){
-		if (is_ready(readyQueue, i, timeElapsed))
-			readyProcesses++;
-	}
-	return readyProcesses;
+    int i;
+    int readyProcesses = 0;
+    for(i = 0; i < readyQueue.length; i++){
+        if (is_ready(readyQueue, i, timeElapsed))
+            readyProcesses++;
+    }
+    return readyProcesses;
 }
 
 int throughput(struct ready_queue readyQueue) {
-	int i;
-	int completeProcesses = 0;
-	for(i = 0; i < readyQueue.length; i++){
-		if (is_complete(readyQueue, i))
-			completeProcesses++;
-	}
-	return completeProcesses;
+    int i;
+    int completeProcesses = 0;
+    for(i = 0; i < readyQueue.length; i++){
+        if (is_complete(readyQueue, i))
+            completeProcesses++;
+    }
+    return completeProcesses;
 }
 
 void exec_round_robin(struct ready_queue readyQueue, struct scheduler_statistics statistics) {
-	int size = readyQueue.length;
-	int currentProcess;
-	int prevProcess = -1;
+    int size = readyQueue.length;
+    int currentProcess;
+    int prevProcess = -1;
     bool queueIsEmpty = (readyQueue.length <= 0);
     float timeElapsed = 0;
     if (queueIsEmpty)
@@ -69,54 +70,56 @@ void exec_round_robin(struct ready_queue readyQueue, struct scheduler_statistics
         printf("\nQueue is empty! Please enlarge queue next time.\n");
     }
     else {
-    	int i = 0;
-    	float totalWait = 0;
-    	float totalTurnAround = 0.0;
-    	float totalResponseTime = 0.0;
-
-    	int completedFlag[readyQueue.length];
-    	memset(completedFlag, 0, readyQueue.length);
-
-    	int visitedFlag[readyQueue.length];
-    	memset(visitedFlag, 0, readyQueue.length);
-
-    	printf("Time Line:\n");
-    	while(timeElapsed < TIME_LIMIT && throughput(readyQueue) < size) {
-
-    		if(is_ready(readyQueue, i, timeElapsed)) {
-    			totalWait += (ready_processes(readyQueue, timeElapsed) - 1);
-    			readyQueue.processes[i].expectedRunTime--;
-
-    			if(readyQueue.processes[i].flagVisited == 0) {
-    				totalResponseTime += (timeElapsed - readyQueue.processes[i].arrivalTime);
-    				readyQueue.processes[i].flagVisited = 1;
-    			}
-    		}
-    		timeElapsed += 1.0;
-    		if (is_complete(readyQueue, i) && readyQueue.processes[i].flagComplete == 0){
-    			totalTurnAround += (timeElapsed - readyQueue.processes[i].arrivalTime);
-    			readyQueue.processes[i].flagComplete = 1;
-    		}
-
-    		currentProcess = readyQueue.processes[i].identifier;
-
-    		i = next_process(readyQueue, timeElapsed, i);
-
-    		if(currentProcess != prevProcess)
-    		{
-    			printf("P%d-", currentProcess);
-    			prevProcess = currentProcess;
-    		}
-    	}
-    	statistics.waiting_time = totalWait/size;
-    	statistics.turnaround_time = totalTurnAround/size;
-    	statistics.response_time = totalResponseTime/size;
-    	printf("\n\nAverage Waiting Time: %f"
-    			"\nAverage Turnaround Time: %f"
-    			"\nAverage Response Time: %f"
-    			"\nThroughput: %d processes were completed"
-    			"\n\n%f quantas have elapsed\n",
-				statistics.waiting_time, statistics.turnaround_time, statistics.response_time, throughput(readyQueue),timeElapsed);
+        int i = 0;
+        float totalWait = 0;
+        float totalTurnAround = 0.0;
+        float totalResponseTime = 0.0;
+        
+        int completedFlag[readyQueue.length];
+        memset(completedFlag, 0, readyQueue.length);
+        
+        int visitedFlag[readyQueue.length];
+        memset(visitedFlag, 0, readyQueue.length);
+        
+        printf("\nTime Line:\n");
+        while(timeElapsed < TIME_LIMIT && throughput(readyQueue) < size) {
+            
+            if(is_ready(readyQueue, i, timeElapsed)) {
+                totalWait += (ready_processes(readyQueue, timeElapsed) - 1);
+                readyQueue.processes[i].expectedRunTime--;
+                
+                if(readyQueue.processes[i].flagVisited == 0) {
+                    totalResponseTime += (timeElapsed - readyQueue.processes[i].arrivalTime);
+                    readyQueue.processes[i].flagVisited = 1;
+                }
+            }
+            timeElapsed += 1.0;
+            if (is_complete(readyQueue, i) && readyQueue.processes[i].flagComplete == 0){
+                totalTurnAround += (timeElapsed - readyQueue.processes[i].arrivalTime);
+                readyQueue.processes[i].flagComplete = 1;
+            }
+            
+            currentProcess = readyQueue.processes[i].identifier;
+            
+            i = next_process(readyQueue, timeElapsed, i);
+            
+            if(currentProcess != prevProcess)
+            {
+                printf("P%d-", currentProcess);
+                prevProcess = currentProcess;
+            }
+        }
+        statistics.waiting_time = totalWait/size;
+        statistics.turnaround_time = totalTurnAround/size;
+        statistics.response_time = totalResponseTime/size;
+        add_to_global_statistics(statistics);
+        printf("\n");
+        /*printf("\n\nAverage Waiting Time: %f"
+         "\nAverage Turnaround Time: %f"
+         "\nAverage Response Time: %f"
+         "\nThroughput: %d processes were completed"
+         "\n\n%f quantas have elapsed\n",
+         statistics.waiting_time, statistics.turnaround_time, statistics.response_time, throughput(readyQueue),timeElapsed);*/
     }
 }
 
--- Assignment 1/Assignment 1/scheduler_srt.c
+++ Assignment 1/Assignment 1/scheduler_srt.c
@@ -2,93 +2,93 @@
 #include "global_statistics.h"
 
 typedef struct List {
-	int length;
-
-	struct simulated_process * processes;
-
+    int length;
+    
+    struct simulated_process * processes;
+    
 } List;
 
 List init_list(int length) {
-	List retList;
-	retList.length = 0;
-	retList.processes = malloc(length * sizeof(struct simulated_process));
-
-	return retList;
+    List retList;
+    retList.length = 0;
+    retList.processes = malloc(length * sizeof(struct simulated_process));
+    
+    return retList;
 }
 
 void delete_list(List* delList) {
-	free(delList->processes);
+    free(delList->processes);
 }
 
 void copy_simulated_process(struct simulated_process* a, struct simulated_process* b) {
-	a->arrivalTime = b->arrivalTime;
-	a->expectedRunTime = b->expectedRunTime;
-	a->priority = b->priority;
+    a->arrivalTime = b->arrivalTime;
+    a->expectedRunTime = b->expectedRunTime;
+    a->priority = b->priority;
 }
 
 void push_process_to_list(List* passQueue, struct simulated_process a) {
-	passQueue->processes[passQueue->length] = a;
-	passQueue->length++;
+    passQueue->processes[passQueue->length] = a;
+    passQueue->length++;
 }
 
 void schedule_SRT(struct ready_queue readyQueue) {
-	int curr_time = 0;
-	int time_slice = 1;
-	int curr_identifier = -1;
-	struct scheduler_statistics stats = new_scheduler_statistics();
-	List runningList = init_list(readyQueue.length);
-	List completedList = init_list(readyQueue.length);
-
-	puts("\nShortest Remaining Time:");
-	printf("Time Line: ");
-
-	while(curr_time < TIME_LIMIT && completedList.length < readyQueue.length) {
-		//Puts a process into the ready queue when it arrives
-		while(readyQueue.processes[0].arrivalTime < curr_time) {
-			push_process_to_list(&runningList, readyQueue.processes[0]);
-			stats.response_time += (curr_time - readyQueue.processes[0].arrivalTime);
-			readyQueue.processes[0].timeEnteredReadyQueue = curr_time;
-			readyQueue.processes++;
-			readyQueue.length--;
-			quick_sort_run_time(runningList.processes, 0, runningList.length - 1);
-		}
-		//Runs the shortest remaining time process from the ready queue
-		if(runningList.length > 0) {
-			//Runs the process
-			runningList.processes[0].expectedRunTime -= time_slice;
-			//Sets the waiting time when the process starts running and stores the time when the process starts running
-			if(runningList.processes[0].flagVisited == 0) {
-				stats.waiting_time += (curr_time - runningList.processes[0].timeEnteredReadyQueue);
-				runningList.processes[0].timeStartsRunning = curr_time;
-			}
-			//Marks the process as visited
-			runningList.processes[0].flagVisited = 1;
-			//Print the current running process
-			if(curr_identifier != runningList.processes[0].identifier)
-				printf("P%i ", runningList.processes[0].identifier);
-			//Marks the currently running process so no repeats in time line if the same process is running
-			if(runningList.processes[0].identifier != 0)
-				curr_identifier = runningList.processes[0].identifier;
-			//Process is finished and is moved to the completed list
-			if(runningList.processes[0].expectedRunTime < 0){
-				push_process_to_list(&completedList, runningList.processes[0]);
-				stats.turnaround_time += (curr_time - completedList.processes[0].timeStartsRunning);
-				runningList.processes++;
-				runningList.length--;
-			}
-		}
-		curr_time++;
-	}
+    int curr_time = 0;
+    int time_slice = 1;
+    int curr_identifier = -1;
+    struct scheduler_statistics stats = new_scheduler_statistics();
+    List runningList = init_list(readyQueue.length);
+    List completedList = init_list(readyQueue.length);
+    
+    puts("\nShortest Remaining Time:");
+    printf("Time Line: ");
+    
+    while(curr_time < TIME_LIMIT && completedList.length < readyQueue.length) {
+        //Puts a process into the ready queue when it arrives
+        while(readyQueue.processes[0].arrivalTime < curr_time) {
+            push_process_to_list(&runningList, readyQueue.processes[0]);
+            stats.response_time += (curr_time - readyQueue.processes[0].arrivalTime);
+            readyQueue.processes[0].timeEnteredReadyQueue = curr_time;
+            readyQueue.processes++;
+            readyQueue.length--;
+            quick_sort_run_time(runningList.processes, 0, runningList.length - 1);
+        }
+        //Runs the shortest remaining time process from the ready queue
+        if(runningList.length > 0) {
+            //Runs the process
+            runningList.processes[0].expectedRunTime -= time_slice;
+            //Sets the waiting time when the process starts running and stores the time when the process starts running
+            if(runningList.processes[0].flagVisited == 0) {
+                stats.waiting_time += (curr_time - runningList.processes[0].timeEnteredReadyQueue);
+                runningList.processes[0].timeStartsRunning = curr_time;
+            }
+            //Marks the process as visited
+            runningList.processes[0].flagVisited = 1;
+            //Print the current running process
+            if(curr_identifier != runningList.processes[0].identifier)
+                printf("P%i ", runningList.processes[0].identifier);
+            //Marks the currently running process so no repeats in time line if the same process is running
+            if(runningList.processes[0].identifier != 0)
+                curr_identifier = runningList.processes[0].identifier;
+            //Process is finished and is moved to the completed list
+            if(runningList.processes[0].expectedRunTime < 0){
+                push_process_to_list(&completedList, runningList.processes[0]);
+                stats.turnaround_time += (curr_time - completedList.processes[0].timeStartsRunning);
+                runningList.processes++;
+                runningList.length--;
+            }
+        }
+        curr_time++;
+    }
     stats.waiting_time /= (completedList.length + runningList.length);
     stats.turnaround_time /= completedList.length;
     stats.response_time /= (completedList.length + runningList.length);
     add_to_global_statistics(stats);
-	puts("");
+    puts("");
     /*printf("Average Waiting Time: %f \nAverage Turnaround Time: %f \nAverage Response Time: %f \nThroughput: %i\n",
-			stats.waiting_time / (completedList.length + runningList.length),
-			stats.turnaround_time / completedList.length,
-			stats.response_time / (completedList.length + runningList.length),
-			completedList.length);*/
-	//delete_list(&runningList);
-	//delete_list(&completedList);
+     stats.waiting_time / (completedList.length + runningList.length),
+     stats.turnaround_time / completedList.length,
+     stats.response_time / (completedList.length + runningList.length),
+     completedList.length);*/
+    //delete_list(&runningList);
+    //delete_list(&completedList);
 }
