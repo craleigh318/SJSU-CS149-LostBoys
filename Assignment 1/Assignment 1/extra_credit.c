ed61f428834186d080ff201b7c14f74db3683452
William Chan <WillWcchan@gmail.com>
2015-02-16 03:51:36 -0800

    Create extra_credit.c

---
+++ Assignment 1/Assignment 1/extra_credit.c
@@ -0,0 +1,15 @@
+
+//Extra credit 
+//Add aging to both HPF nonpreemptive and HPF preemptive. After a process has waited
+//for 5 quanta at a priority level, bump it up to the next higher level.
+
+
+//void extra_credit_preemptive(struct ready_queue readyQueue1,struct ready_queue readyQueue2,struct ready_queue readyQueue3,struct ready_queue readyQueue4) {
+//			extra_credit_round_robin(readyQueue1,readyQueue2,readyQueue3,readyQueue4); // Send the whole 4 priority queues at once to the method
+//}
+
+
+//void extra_credit_nonpreemptive(struct ready_queue readyQueue1,struct ready_queue readyQueue2,struct ready_queue readyQueue3,struct ready_queue readyQueue4)
+//{
+//			extra_credit_scheduler_fcfs(readyQueue1,readyQueue2,readyQueue3,readyQueue4);
+//}
