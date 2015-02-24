f63cf33507e9022842ba06ff47187b7b413c8113
William Chan <WillWcchan@gmail.com>
2015-02-24 14:18:53 -0800

    Update sections.c

--- Assignment 3/Assignment 3/sections.c
+++ Assignment 3/Assignment 3/sections.c
@@ -13,6 +13,19 @@ Sections newSections() {
     return new_Sections;
 }
 
+/*
+THREAD 1                        THREAD 2
+pthread_mutex_lock (&mut);      
+                                pthread_mutex_lock (&mut); 
+a = data;                       /* blocked */
+// a++;                            /* blocked */
+//data = a;                       /* blocked */
+// pthread_mutex_unlock (&mut);    /* blocked */
+                                // b = data;
+                                // b--;
+                                // data = b;
+                                //pthread_mutex_unlock (&mut);
+*/
 bool add_student_to_section(Sections* section, Student student) {
 	if(section->seatsLeft > 0) {
 		section->addedStudents[SIZE_OF_CLASS - section->seatsLeft] = student;
