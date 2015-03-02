5f2eb59cf1834c37ae6ab16df57871d43d32a35c
Christopher Raleigh <craleigh318@comcast.net>
2015-03-01 18:27:41 -0800

    Fixed student.c.

--- Assignment 3/Assignment 3/student.c
+++ Assignment 3/Assignment 3/student.c
@@ -11,14 +11,14 @@
 #include "print_queue.h"
 
 int get_arrival_time() {
-	return rand() % END_TIME; // Gets a number from 0 to 120 seconds
+    return rand() % END_TIME; // Gets a number from 0 to 120 seconds
 }
 
 Student new_student(int idNumber) {
     Student newStudent = {
         idNumber,
         get_random_student_type(), // returns a number 0 - 2 of student type
-        get_random_section(), // section.c class returns 0 - 4 of sections 
+        get_random_section(), // section.c class returns 0 - 4 of sections
         get_arrival_time() // 0 to 120 seconds
     };
     return newStudent;
@@ -43,41 +43,41 @@ void student_to_string(Student student, char * destination) {
 
 void* process_student(void* threadId)
 {
-	ThreadParams* params = threadId;
-	Student student = params->student;
-	print_student(student);
-
-	Sections* addingSection;
-	if(student.sectionNum == 1)
-        add_student_to_section(addingSection,student); // Sends the student to the section they want to apply to
-        // addingSection = params->s1;
-	else if(student.sectionNum == 2)
-         add_student_to_section(addingSection,student);
-        // addingSection = params->s2;
-	else if(student.sectionNum == 3)
-         add_student_to_section(addingSection,student);
-    // addingSection = params->s3;
-	else if(student.sectionNum == 4) {
-         add_student_to_section(addingSection,student);
+    ThreadParams* params = threadId;
+    Student student = params->student;
+    print_student(student);
+    
+    Sections* addingSection = NULL;
+    if(student.sectionNum == 1)
+        addingSection = params->s1;
+    else if(student.sectionNum == 2)
+        addingSection = params->s2;
+    else if(student.sectionNum == 3)
+        addingSection = params->s3;
+    else if(student.sectionNum == 4) {
         addingSection = params->s1;
         // Steve said change alogrithm
-	}
-
-	pthread_mutex_lock(&addingSection->lock);
-	add_student_to_section(addingSection, student);
-	sleep(params->processTime);
-	//pthread_mutex_unlock(&addingSection->lock);
-	free(params);
-	pthread_exit(NULL);
+    }
+    else
+    {
+        addingSection = NULL;
+    }
+    add_student_to_section(addingSection,student); // Sends the student to the section they want to apply to
+    pthread_mutex_lock(&addingSection->lock);
+    add_student_to_section(addingSection, student);
+    sleep(params->processTime);
+    //pthread_mutex_unlock(&addingSection->lock);
+    free(params);
+    pthread_exit(NULL);
 }
 
 void print_student(Student student)
-    {
+{
     char studentString[65]; // A string with a length of 65 characters
     sprintf(studentString, "Student %2i   Type: %i   Section: %i   Arrival: %i",
-			student.idNumber,
-			student.type,
-			student.sectionNum,
-			student.arrivalTime);
-        print_pq(studentString); // sends the studentString to the print queue
-    }
+            student.idNumber,
+            student.type,
+            student.sectionNum,
+            student.arrivalTime);
+    print_pq(studentString); // sends the studentString to the print queue
+}
