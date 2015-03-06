2032ae2562257499947efeba34feefc398afb5c8
Christopher Raleigh <craleigh318@comcast.net>
2015-03-05 22:38:43 -0800

    Added Assignment 4.

---
+++ Assignment 4/Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4
+//
+//  Created by Christopher Raleigh on 2015/05/03.
+//  Copyright (c) 2015 Lost Boys. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
