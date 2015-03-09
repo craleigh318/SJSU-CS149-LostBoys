cd32eb95d534afda547936c384970ef732cc4b12
steviewanger <steviewanger@gmail.com>
2015-03-09 14:11:13 -0700

    Added NextFit Algorithm Files

---
+++ Assignment 4/Assignment 4/NextFitSwapping.cpp
@@ -0,0 +1,10 @@
+/*
+ * NextFitSwapping.cpp
+ *
+ *  Created on: Mar 9, 2015
+ *      Author: jr
+ */
+
+
+
+
---
+++ Assignment 4/Assignment 4/NextFitSwapping.h
@@ -0,0 +1,15 @@
+/*
+ * NextFitSwapping.h
+ *
+ *  Created on: Mar 9, 2015
+ *      Author: jr
+ */
+
+#ifndef NEXTFITSWAPPING_H_
+#define NEXTFITSWAPPING_H_
+
+
+
+
+
+#endif /* NEXTFITSWAPPING_H_ */
