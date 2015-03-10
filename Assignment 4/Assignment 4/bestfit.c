6dcf9ad7326c9f70ad2906d797aa72c6a5c9e1af
WillWcchan@gmail.com <WillWcchan@gmail.com>
2015-03-10 15:27:28 -0700

    Add code later, Xcode not working

---
+++ Assignment 4/Assignment 4/bestfit.c
@@ -0,0 +1,11 @@
+//
+//  bestift.c
+//  Assignment 4
+//
+//  Created by William Chan on 2015/05/03.
+//  Copyright (c) 2015 Lost Boys. All rights reserved.
+//
+void runBestFit()
+{
+	return;
+}
--- Assignment 4/Assignment 4/bestfit.h
+++ Assignment 4/Assignment 4/bestfit.h
@@ -5,3 +5,4 @@
 //  Created by William Chan on 2015/05/03.
 //  Copyright (c) 2015 Lost Boys. All rights reserved.
 //
+void runBestFit();
