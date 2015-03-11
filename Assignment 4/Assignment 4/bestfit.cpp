ce8b84c028b0e13df30bbe012906c22325617afb
William Chan <WillWcchan@gmail.com>
2015-03-11 15:07:46 -0700

    Update bestift.c

--- Assignment 4/Assignment 4/bestfit.cpp
+++ Assignment 4/Assignment 4/bestfit.cpp
@@ -10,6 +10,20 @@
 
 void runBestFit()
 {
-    Process *memory = newMainMemory(); // Gets a new block of main memory as an array of 100 process pointers 
-    
+    Process **memory = newMainMemory(); // Gets a new block of main memory as an array of 100 process pointers 
+    std::vector<Partition *> holes = getHolesInMemory(memory);  // Get and find holes in memory
+    int startOfVector = 0, startOfMain = 0;
+    while(startOfMain < 100)
+    {
+        Process *process = newProcess(); // Gets a randomly generated new process  
+        startOfMain += process.getProcessSize(); // Gets the process size and increment the index of the main memory
+        while(startOfVector < holes.size()) // Start adding processes in the vector 
+        {
+            if ( holes.at(startOfVector) == NULL && startOfVector < process.getProcessSize() ) // if the vector at that index is null ( or  0 )
+            {
+                holes.at(i) = process; // Adds that process into the array of that index 
+                startOfVector++;  // moves the index to the start 
+            }
+        }
+    }
 }
