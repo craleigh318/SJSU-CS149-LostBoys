fc96e753e8484b9bf3c7637dc250e3de6bfbf233
William Chan <WillWcchan@gmail.com>
2015-03-11 02:41:22 -0700

    "updated my bestfit.c"

--- Assignment 4/Assignment 4/bestfit.c
+++ Assignment 4/Assignment 4/bestfit.c
@@ -8,8 +8,14 @@
 
 #include "bestfit.h"
 
+// Best Fit - search the Linked Lists of memory segments for the smallest hole where the process will fit
+// Slower: longer search
+// Rsult: More wasted memory
+// Fills memory with tiny useless holes
+
 void runBestFit()
 {
     Process **mem = newMainMemory(); // A block of main memory is an array of 100 process pointers
     Process *process = newProcess(); // A randomly simulated process with size and duration
+    
 }
