8a8efc7aac556fe8998ee8478f69bf8c504ee2c4
steviewanger <steviewanger@gmail.com>
2015-03-13 01:18:53 -0700

    Added deleting process if finished

--- Assignment 4/Assignment 4/Swapping.cpp
+++ Assignment 4/Assignment 4/Swapping.cpp
@@ -17,11 +17,31 @@ void swapWithAlgorithm(std::queue<Process *> processQueue,
     MainMemory memory;
     int currentTime;
     for (currentTime = 0; currentTime < TIME_LIMIT; ++currentTime) {
-        Process * nextProcess = processQueue.front();
-        bool processAdded = algorithm(&memory, nextProcess);
-        if (processAdded) {
-        	nextProcess->setTimeAdded(currentTime);
-            processQueue.pop();
+        bool processAdded = true;
+        bool printLine = false;
+        while(processAdded) {
+            Process * nextProcess = processQueue.front();
+        	processAdded = algorithm(&memory, nextProcess);
+			if (processAdded) {
+				nextProcess->setTimeAdded(currentTime);
+				processQueue.pop();
+				printLine = true;
+			}
+        }
+        if(printLine) {
+			std::cout << "Second " << currentTime << '\n';
+			memory.print();
+			std::cout << '\n';
+        }
+        printLine = false;
+        std::vector<Partition> allProcess = Partition::getPartitionsInMemoryWithProcesses(&memory);
+        for(std::vector<Process*>::size_type i = 0; i != allProcess.size(); i++) {
+        	if(allProcess[i].getProcess()->getDuration() + allProcess[i].getProcess()->getTimeAdded() == currentTime) {
+        		allProcess[i].removeProcess();
+        		printLine = true;
+        	}
+        }
+        if(printLine) {
             std::cout << "Second " << currentTime << '\n';
             memory.print();
             std::cout << '\n';
@@ -32,7 +52,7 @@ void swapWithAlgorithm(std::queue<Process *> processQueue,
 void swappingStart() {
     std::cout << "Swapping:\n";
     std::queue<Process *> orderedProcesses;
-    const int queueSize = 26;
+    const int queueSize = 100;
     int i;
     for (i = 0; i < queueSize; ++i) {
         Process * newProcess = new Process();
@@ -45,13 +65,13 @@ void swappingStart() {
     swapWithAlgorithm(orderedProcesses, &startFirstFitSwapping);
 
     std::cout << "Next Fit:\n";
-    swapWithAlgorithm(orderedProcesses, &startNextFit);
+    //swapWithAlgorithm(orderedProcesses, &startNextFit);
 
     std::cout << "Best Fit:\n";
-    swapWithAlgorithm(orderedProcesses, &startBestFit);
+    //swapWithAlgorithm(orderedProcesses, &startBestFit);
 
     std::cout << "Worst Fit:\n";
-    swapWithAlgorithm(orderedProcesses, &startWorstFit);
+    //swapWithAlgorithm(orderedProcesses, &startWorstFit);
     // Deleting processes when done.
     for (i = 0; i < queueSize; ++i) {
         Process * nextProcess = orderedProcesses.front();
