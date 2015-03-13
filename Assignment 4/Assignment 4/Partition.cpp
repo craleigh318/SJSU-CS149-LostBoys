d340fb51a04aa9dbfb7898b6cb68b10325e48c98
Christopher Raleigh <craleigh318@comcast.net>
2015-03-13 00:10:45 -0700

    Changed to getPartitionsInMemoryWithProcesses.

--- Assignment 4/Assignment 4/Partition.cpp
+++ Assignment 4/Assignment 4/Partition.cpp
@@ -59,14 +59,14 @@ std::vector<Partition> Partition::getHolesInMemory(MainMemory * mainMemory) {
     return memoryHoles;
 }
 
-std::vector<const Process *> Partition::getProcessesInMemory(MainMemory * mainMemory) {
-    std::vector<const Process *> processes;
+std::vector<Partition> Partition::getPartitionsInMemoryWithProcesses(MainMemory * mainMemory) {
+    std::vector<Partition> processes;
     std::vector<Partition> partitions = getPartitionsInMemory(mainMemory);
     int i;
     for (i = 0; i < partitions.size(); ++i) {
         Partition currentPartition = partitions.at(i);
         if (!currentPartition.getProcess()) {
-            processes.push_back(currentPartition.getProcess());
+            processes.push_back(currentPartition);
         }
     }
     return processes;
