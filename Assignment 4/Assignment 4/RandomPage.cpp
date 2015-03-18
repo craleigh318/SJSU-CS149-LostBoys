48a71836fb9967557b32f444fa209b6f87fce6b7
will <will@will-VirtualBox>
2015-03-18 16:40:42 -0700

    "Added more code into RandomPage , somewhat complete"

--- Assignment 4/Assignment 4/RandomPage.cpp
+++ Assignment 4/Assignment 4/RandomPage.cpp
@@ -1,1 +1,14 @@
+#include "RandomPage.h"
 
+RandomPage::RandomPage(PageFrameSet underlyingContainer) :
+SwappingAlgorithm(underlyingContainer)
+{
+}
+
+Page * RandomPage::addPage(Page * page) {
+    PageFrameSet * frames = getUnderlyingContainer(); // Get the 4 physical page memory 
+    int randomPage = rand() % frames -> getSize(); // Gets a random page from the frames to remove
+    Page * victim = frames->getPage(randomPage); // Getting the actual page and accessing it 
+    frames->setPage(randomPage, NULL); 
+    return victim;
+}
