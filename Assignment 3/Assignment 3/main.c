67ef956eb84111ba44b455fc0c538069228143da
William Chan <WillWcchan@gmail.com>
2015-03-21 00:33:09 -0700

    Update main.c

--- Assignment 3/Assignment 3/main.c
+++ Assignment 3/Assignment 3/main.c
@@ -20,21 +20,41 @@ void create_time_stamp(int time, char * destination) {
 
 void print_results()
 {
-    char str[30], strr[30];
+    char str[30], strr[30], strrr[30];
     char *str1 = "students enrolled.";
     char *str2 = "students dropped.";
+    char *str3 = "students arrived late.";
     int add = getStudentAdded();
     int drop = getStudentDropped();
+    int late = 75 - (add + drop);
     sprintf(str, "%i %s " , add , str1);
     sprintf(strr,"%i %s " , drop, str2);
+    sprintf(strrr,"%i %s " , late, str3);
     print_pq(str);
     print_pq(strr);
+    print_pq(strrr);
     int totalTime = getTotal();
-    char num2[50];
-    sprintf(num2,"%i : total turnaround time seconds", totalTime);
-    print_pq(num2);
+    int totalGSTime = getTotalGS();
+    int totalRSTime = getTotalRS();
+    int totalEETime = getTotalEE();
+
+    float averageTimeGS = ((float)totalGSTime)/75.0f;
+    char num4[100];
+	sprintf(num4, "%f : average turnaround time for GS Queue seconds" , averageTimeGS);
+	print_pq(num4);
+
+    float averageTimeRS = ((float)totalRSTime)/75.0f;
+    char num3[100];
+	sprintf(num3, "%f : average turnaround time for RS Queue seconds" , averageTimeRS);
+	print_pq(num3);
+
+    float averageTimeEE = ((float)totalEETime)/75.0f;
+    char num2[100];
+	sprintf(num2, "%f : average turnaround time for EE Queue seconds" , averageTimeEE);
+	print_pq(num2);
+
     float averageTime = ((float)totalTime)/75.0f;
-    char num[50];
+    char num[100];
     sprintf(num, "%f : average turnaround time seconds" , averageTime);
     print_pq(num);
 }
@@ -52,7 +72,7 @@ void start_enrollment_process() {
     
     int j;
     for (j = 0; j < MAX_STUDENTS; ++j) {
-        push_student_queue(&mainStudentQueue, studentList[j], currTime);
+        push_student_queue(&mainStudentQueue, studentList[j]);
     }
     StudentQueue rsQueue = new_student_queue();
     StudentQueue gsQueue = new_student_queue();
@@ -68,24 +88,24 @@ void start_enrollment_process() {
         {
             Student currStudent = pop_student_queue(&mainStudentQueue);
             if (currStudent.type == gs) {
-                addTotal(push_student_queue(&gsQueue, currStudent, currTime));
+                (push_student_queue(&gsQueue, currStudent));
             }
             else if (currStudent.type == rs) 	{
-                addTotal(push_student_queue(&rsQueue, currStudent, currTime));
+                (push_student_queue(&rsQueue, currStudent));
             }
             else if (currStudent.type == ee) 	{
-                addTotal(push_student_queue(&eeQueue, currStudent, currTime));
+                (push_student_queue(&eeQueue, currStudent));
             }
             else
                 print_pq("ERROR: Invalid Student type");
         }
         
         if(gsQueue.length > 0)
-            process_student_queue(&gsQueue, &sect1, &sect2, &sect3, studentsThread);
+            process_student_queue(&gsQueue, &sect1, &sect2, &sect3, studentsThread,currTime);
         if(rsQueue.length > 0)
-            process_student_queue(&rsQueue, &sect1, &sect2, &sect3, studentsThread);
+            process_student_queue(&rsQueue, &sect1, &sect2, &sect3, studentsThread,currTime);
         if(eeQueue.length > 0)
-            process_student_queue(&eeQueue, &sect1, &sect2, &sect3, studentsThread);
+            process_student_queue(&eeQueue, &sect1, &sect2, &sect3, studentsThread,currTime);
         
         char complete[10];
         create_time_stamp(currTime, complete);
@@ -123,4 +143,3 @@ int main(int argc, const char * argv[]) {
     pthread_exit(NULL);
     return 0;
 }
-// That is all
