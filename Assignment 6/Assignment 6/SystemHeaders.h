65d7ea9abadad617874cff4c5219a9d35901cce6
Christopher Raleigh <craleigh318@comcast.net>
2015-04-15 18:54:18 -0700

    Tested pipe for Assignment 6.

---
+++ Assignment 6/Assignment 6/SystemHeaders.h
@@ -0,0 +1,13 @@
+//
+//  SystemHeaders.h
+//  Assignment 6
+//
+//  Created by Christopher Raleigh on 2015/15/04.
+//  Copyright (c) 2015 Lost Boys. All rights reserved.
+//
+
+#ifndef Assignment_6_SystemHeaders_h
+#define Assignment_6_SystemHeaders_h
+#include <stdio.h>
+#include <unistd.h>
+#endif
--- Assignment 6/Assignment 6/main.c
+++ Assignment 6/Assignment 6/main.c
@@ -6,10 +6,20 @@
 //  Copyright (c) 2015 Lost Boys. All rights reserved.
 //
 
-#include <stdio.h>
+#include "SystemHeaders.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    /*
+     Create 5 pipes.
+     Each pipe has 2 integers.
+     */
+    int pipes[2][5];
+    if (pipe(pipes[0])) {
+        printf("Pipe 0 failed.");
+    } else {
+        printf("Pipe 0 succeeded");
+    }
     return 0;
 }
