febd3507f9052cfe0224cdc4829573e0e861172d
Christopher Raleigh <craleigh318@comcast.net>
2015-04-15 17:40:45 -0700

    Added Assignment 6.

---
+++ Assignment 6/Assignment 6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment 6
+//
+//  Created by Christopher Raleigh on 2015/15/04.
+//  Copyright (c) 2015 Lost Boys. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
