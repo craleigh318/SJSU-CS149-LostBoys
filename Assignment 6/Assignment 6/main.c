bb444814fcd458c0a5904d62f788a0b2493af9c4
Christopher Raleigh <craleigh318@comcast.net>
2015-04-19 19:21:36 -0700

    Added print statements.

--- Assignment 6/Assignment 6/main.c
+++ Assignment 6/Assignment 6/main.c
@@ -8,6 +8,15 @@
 
 #include "SystemHeaders.h"
 
+void sendMessages(int * pipe) {
+    /*
+     pipe[0] is for reading.
+     pipe[1] is for writing.
+     */
+    pid_t childID = getpid();
+    printf("This is pipe %d.\n", childID);
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
@@ -16,33 +25,15 @@ int main(int argc, const char * argv[]) {
      Each pipe has 2 integers.
      */
     int pipes[2][5];
-    if (pipe(pipes[0])) {
-        printf("Pipe 0 failed.");
-    } else {
-        printf("Pipe 0 succeeded");
-    }
     pid_t parentID = getpid();
-    if (parentID != fork()) {
-        /*
-         Child 1.
-         */
-        
-    } else if (parentID != fork()) {
-        /*
-         Child 2.
-         */
-    } else if (parentID != fork()) {
-        /*
-         Child 3.
-         */
-    } else if (parentID != fork()) {
-        /*
-         Child 4.
-         */
-    } else if (parentID != fork()) {
-        /*
-         Child 5.
-         */
+    const int NUM_CHILDREN = 5;
+    int i = 0;
+    for (i = 0; i < NUM_CHILDREN; ++i) {
+        fork();
+        if (parentID != getpid()) {
+            sendMessages(pipes[i]);
+            break;
+        }
     }
     return 0;
 }
